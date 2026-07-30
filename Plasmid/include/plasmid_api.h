dcdae0f39589f8bf6b06dd2bf9561c664f369a53
u0068 <g3nio15@gmail.com>
2026-07-30 02:49:16 +0100

    improved AddCellCall

--- Plasmid/include/plasmid_api.h
+++ Plasmid/include/plasmid_api.h
@@ -1,6 +1,8 @@
 #pragma once
+
 #include "hook_manager.h"
 #include "nucleus_interface.h"
+#include "generated/properties/material_t.h"
 
 namespace APIUtil
 {
@@ -8,54 +10,11 @@ namespace APIUtil
     #include "generated/game_functions/plasmid_api.h"
     #include "generated/resolve_data.h"
 
-    enum InitMatAddType
-    {
-        complex = 0,
-        simple = 1,
-        function = 2
-    };
-
-    struct AddCellCallSimple
-    {
-        int copyFrom = 1;
-        real_4 color = { 0.f, 0.f, 0.f, 1.f };
-        void (*physics_update_fn)(struct cell*) = nullptr;
-        void (*force_update_fn)(struct cell*) = nullptr;
-        void (*electric_update_fn)(struct cell*) = nullptr;
-        void (*connection_update_fn)(struct cell*) = nullptr;
-        void (*brain_fn)(struct cell*) = nullptr;
-        void (*destroyed_fn)(struct cell*) = nullptr;
-    };
-
-    struct AddCellCallEx
-    {
-        uint copyFrom;
-        void(*overwrite)(material_t*);
-        uint newId;
-    };
-
-    struct CellFunctionOverwrite
-    {
-        void (*physics_update_fn)(struct cell*) = nullptr;
-        void (*force_update_fn)(struct cell*) = nullptr;
-        void (*electric_update_fn)(struct cell*) = nullptr;
-        void (*connection_update_fn)(struct cell*) = nullptr;
-        void (*brain_fn)(struct cell*) = nullptr;
-        void (*destroyed_fn)(struct cell*) = nullptr;
-        uint idx = 0;
-    };
-
     struct AddCellCall
     {
-        APIUtil::InitMatAddType type = APIUtil::InitMatAddType::simple;
-
-        union context {
-           AddCellCallSimple simple;
-           AddCellCallEx extensive;
-           CellFunctionOverwrite function;
-        };
-
-        context cell;
+        material_t_properties properties;
+        int index{-1};
+        int copyFrom{-1};
     };
 
     struct UpdateWorkCall
@@ -67,7 +26,7 @@ namespace APIUtil
     static std::vector<AddCellCall> cells2add;
     static std::vector<UpdateWorkCall> update_cells_work_append;
 
-    static uint GetCellIdxdByStr(const char* cell)
+    static uint StrToCellIdx(const char* cell)
     {
         uint search = str_to_id(const_cast<char*>(cell));
         for (int i = 1; i < n_materials; i++)
@@ -77,7 +36,7 @@ namespace APIUtil
         }
         return 0;
     }
-    static uint GetCellIdxById(uint search)
+    static uint IdToCellIdx(uint search)
     {
         for (int i = 1; i < n_materials; i++)
         {
@@ -87,41 +46,9 @@ namespace APIUtil
         return 0;
     }
 
-    static void QueueAddCell(const AddCellCall& addcell)
+    static void QueueAddCell(const AddCellCall& add_cell)
     {
-        cells2add.push_back(addcell);
-    }
-
-    static void AddCell(
-        real_4 color,
-        int copyFrom = 1,
-        void (*physics_update_fn)(struct cell*) = nullptr,
-        void (*force_update_fn)(struct cell*) = nullptr,
-        void (*electric_update_fn)(struct cell*) = nullptr,
-        void (*connection_update_fn)(struct cell*) = nullptr,
-        void (*brain_fn)(struct cell*) = nullptr,
-        void (*destroyed_fn)(struct cell*) = nullptr
-    )
-    {
-        if (n_materials >= 2048)
-        {
-           printf("Material count is at maximum !\n");
-           return;
-        }
-
-        materials_list[n_materials] = materials_list[copyFrom];
-        materials_list[n_materials].base_cost = float(n_materials);
-        materials_list[n_materials].movement_force = 0.5f;
-        materials_list[n_materials].base_color = color;
-
-        materials_list[n_materials].physics_update_fn = physics_update_fn;
-        materials_list[n_materials].force_update_fn = force_update_fn;
-        materials_list[n_materials].electric_update_fn = electric_update_fn;
-        materials_list[n_materials].connection_update_fn = connection_update_fn;
-        materials_list[n_materials].brain_fn = brain_fn;
-        materials_list[n_materials].destroyed_fn = destroyed_fn;
-
-        n_materials++;
+        cells2add.push_back(add_cell);
     }
 
     inline void AddAllCells()
@@ -130,56 +57,23 @@ namespace APIUtil
         int* threadsafe = static_cast<int*>(TlsGetValue(tls_index));
         if (*threadsafe == 0)
         {
-           for (auto & i : cells2add)
-           {
-              if (i.type == APIUtil::InitMatAddType::simple)
-              {
-                 APIUtil::AddCell(
-                    i.cell.simple.color,
-                    i.cell.simple.copyFrom,
-                    i.cell.simple.physics_update_fn,
-                    i.cell.simple.force_update_fn,
-                    i.cell.simple.connection_update_fn,
-                    i.cell.simple.brain_fn,
-                    i.cell.simple.destroyed_fn
-                 );
-              }
-              else if (i.type == APIUtil::InitMatAddType::complex)
-              {
-                 if (n_materials >= 2048)
-                 {
+            for (auto & cell : cells2add)
+            {
+                if (n_materials >= 2048)
+                {
                     printf("Cell count is at maximum !\n");
                     return;
-                 }
+                }
 
-                 uint idx = GetCellIdxById(i.cell.extensive.copyFrom);
-                 if (!idx)
-                    continue;
+                uint index = cell.index == -1 ? n_materials : cell.index;
+                uint copy_from = cell.copyFrom == -1 ? (cell.index == -1 ? 1 : cell.index) : cell.copyFrom;
 
-                 materials_list[n_materials] = materials_list[i.cell.extensive.copyFrom];
-                 materials_list[n_materials].id = i.cell.extensive.newId;
-                 i.cell.extensive.overwrite(&(materials_list[n_materials]));
-                 n_materials++;
-              }
-              else
-              {
-                 /*
-                 we have to search for a cell again because vanilla cells will never be loaded at the start of game
-                 order of vanilla cells can change per version, if modder wants a faster method can add themselves easily
-                 */
-
-                 uint idx = GetCellIdxById(i.cell.function.idx);
-                 if (!idx)
-                    continue;
-
-                 materials_list[idx].physics_update_fn = i.cell.function.physics_update_fn;
-                 materials_list[idx].force_update_fn = i.cell.function.force_update_fn;
-                 materials_list[idx].electric_update_fn = i.cell.function.electric_update_fn;
-                 materials_list[idx].connection_update_fn = i.cell.function.connection_update_fn;
-                 materials_list[idx].brain_fn = i.cell.function.brain_fn;
-                 materials_list[idx].destroyed_fn = i.cell.function.destroyed_fn;
-              }
-           }
+                materials_list[index] = materials_list[copy_from];
+                materials_list[index].max_health = index;
+                ApplyProperties(materials_list[index], cell.properties);
+                if (cell.index == -1)
+                    n_materials++;
+            }
         }
     }
 
@@ -207,19 +101,6 @@ namespace APIUtil
         Hook("init_materials_list", AddAllCellsHook);
         // Hook("update_cells", AddAllWorkHook);
     };
-
-    static void OverwriteCellFunction(const char* cell, const CellFunctionOverwrite& overwrite)
-    {
-        uint idx = str_to_id(const_cast<char*>(cell));
-        if (idx)
-        {
-           AddCellCall ncall{};
-           ncall.type = InitMatAddType::function;
-           ncall.cell.function = overwrite;
-           ncall.cell.function.idx = idx;
-           cells2add.push_back(ncall);
-        }
-    }
 };
 
 extern"C" __declspec(dllexport)
--- Plasmid/mod_src/cell_example_mod/main.cpp
+++ Plasmid/mod_src/cell_example_mod/main.cpp
@@ -3,11 +3,8 @@
 extern"C" __declspec(dllexport)
 void mod_main()
 {
-    // Halve the density of lightweight cell
-    APIUtil::materials_list[APIUtil::GetCellIdxdByStr("LIGT")].density *= 0.5;
-
-    APIUtil::AddCellCall newCell{};
-    newCell.cell.simple.copyFrom = APIUtil::GetCellIdxdByStr("TRG0");
-    newCell.cell.simple.color = {0.5, 0.6, 0.7, 1.0};
+    APIUtil::AddCellCall newCell;
+    newCell.copyFrom = APIUtil::StrToCellIdx("HARD");
+    newCell.properties.base_color = real_4{0.0, 0.0, 0.0, 1.0};
     QueueAddCell(newCell);
 }
