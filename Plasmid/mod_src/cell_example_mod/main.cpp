fa6d0c19947046c38a877dc93759ba6ed9ed2eea
u0068 <g3nio15@gmail.com>
2026-08-03 01:11:14 +0100

    replaced AddCell helper with OnInitMaterials hook with wrapper

--- Plasmid/mod_src/cell_example_mod/main.cpp
+++ Plasmid/mod_src/cell_example_mod/main.cpp
@@ -1,27 +1,19 @@
 #include "plasmid_api.h"
 
-// void hook()
-// {
-//     Next<void>();
-// }
+void APIUtil::OnInitMaterials()
+{
+    material_t* mats = materials_list;
+
+    material_t cell = mats[CellRef{"BODY"}.GetIndex()];
+    cell.name = (char*)"Cell Name";
+    cell.id = CellRef{"NWCL"};
+    cell.base_color = real_4{0.0, 0.0, 0.0, 1.0};
+    AddCellDescription(CellRef{"NWCL"}, (char*)"Cell Description");
+    int index = n_materials++;
+    mats[index] = cell;
+}
+
 extern"C" __declspec(dllexport)
 void mod_main()
 {
-    // Hook<"set_lava_walls">(hook);
-
-    APIUtil::AddCellCall newCell;
-    newCell.copyFrom = APIUtil::CellRef{"BODY"};
-    newCell.props.name = const_cast<char *>("Cell Name");
-    newCell.props.desc = const_cast<char *>("Cell Description");
-    newCell.props.id = APIUtil::CellRef{"NWCL\0"};
-    newCell.props.base_color = real_4{0.0, 0.0, 0.0, 1.0};
-    QueueAddCell(newCell);
-
-
-    newCell.copyFrom = APIUtil::CellRef{"SWIM"};
-    newCell.props.name = const_cast<char *>("Cell Name2");
-    newCell.props.desc = const_cast<char *>("Cell Description2");
-    newCell.props.id = APIUtil::CellRef{"NWC2\0"};
-    newCell.props.base_color = real_4{1.0, 0.0, 0.0, 1.0};
-    QueueAddCell(newCell);
 }
