48c33814d09f2ec9168f4bc123a5b418f97761ec
u0068 <g3nio15@gmail.com>
2026-08-03 23:52:55 +0100

    added logging to file plasmid_log.txt

--- Plasmid/include/nucleus_interface.h
+++ Plasmid/include/nucleus_interface.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <unordered_map>
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <format>
 
 struct HookChainBase
 {
@@ -18,10 +21,18 @@ struct NucleusRuntimeAPI
 
 inline NucleusRuntimeAPI* nucleus;
 
+// ====== Other shared stuff ======
+
+inline std::string log_filename = "plasmid_log.txt";
+inline std::ofstream log_file(log_filename);
+
 template<typename... Args>
 void LogTemplate(const char* name, Args... args)
 {
-    char buffer[2048];
-    sprintf_s(buffer,  args...);
-    printf("[%s]\n  %s", name, buffer);
+    char buffer1[2048];
+    sprintf_s(buffer1,  args...);
+    char buffer2[2048];
+    sprintf_s(buffer2, "[%s]\n  %s", name, buffer1);
+    printf(buffer2);
+    log_file << buffer2;
 }
