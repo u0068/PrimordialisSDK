12350496f9a27b1a8a8b63fe91a7707d546fb0e9
u0068 <g3nio15@gmail.com>
2026-09-08 02:28:31 +0100

    made aerogel less janky

--- mod_src/official/cpp/MoreCells/materials/aerogel_cell.h
+++ mod_src/official/cpp/MoreCells/materials/aerogel_cell.h
@@ -4,6 +4,7 @@
 inline void AddAeroGelCell()
 {
     auto material = P::materials_list[P::CellRef{"Lightweight cell"}.GetIndex()];
+    material.is_hard = true;
     material.density = 0.1f;
     material.base_cost = 2.0f;
     material.max_health = 0.1f;
@@ -11,12 +12,13 @@ inline void AddAeroGelCell()
     material.growth_rate *= 0.5f;
     material.transfer_rate *= 0.05f;
     material.regen *= 0.5f;
-    material.max_angular_force *= 0.1f;
-    material.max_radial_force *= 0.1f;
-    material.radial_compliance *= 0.1f;
-    material.angular_compliance *= 0.1f;
+    material.max_angular_force = 0.05f;
+    material.max_radial_force = 0.15f;
+    material.radial_compliance = 0.0f;
+    material.angular_compliance = 0.0f;
     material.plasticity = 0.01f;
     material.restitution = 0.9f;
+    material.drag = 0.1f;
     material.heat_conductivity = 0.0f;
     material.heat_capacity = 10.0f;
     material.inv_heat_capacity = 1.0f / material.heat_capacity;
