d40713bbf3ac83ce265ccfb036f3d77c821304a7
u0068 <g3nio15@gmail.com>
2026-09-13 22:21:57 +0100

    added ability trigger variants

---
+++ mod_src/official/cpp/MoreCells/materials/trigger_variants.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <generated/game_functions/creatures.h>
+#include "plasmid_api.h"
+
+void __cdecl NegativeTrigger(P::cell* cell)
+{
+    auto body = P::get_living_body(cell->body_id);
+    if (body->brain.abilities[0])
+    {
+        float output = -cell->voltage_multiplier;
+        if (abs(cell->voltage) < abs(output)) {
+            cell->voltage = output;
+        }
+    }
+}
+
+void __cdecl InvertedTrigger(P::cell* cell)
+{
+    auto body = P::get_living_body(cell->body_id);
+    if (not body->brain.abilities[0])
+    {
+        float output = cell->voltage_multiplier;
+        if (abs(cell->voltage) < abs(output)) {
+            cell->voltage = output;
+        }
+    }
+}
+
+inline void AddTriggerVariants()
+{
+    P::material_t material{};
+
+    material = P::materials_list[P::CellRef{"Ability trigger cell 1"}.GetIndex()];
+    material.electric_update_fn = NegativeTrigger;
+    material.base_color = {0.25f, 1.0f, 1.0f, 1.0f}; // Cyan
+    P::SetCellNameAndDesc(material, "Negative ability trigger cell", "Produces -1V when ability trigger is pressed.");
+    P::materials_list[P::n_materials++] = material;
+
+    material = P::materials_list[P::CellRef{"Ability trigger cell 1"}.GetIndex()];
+    material.electric_update_fn = InvertedTrigger;
+    material.base_color = {1.0f, 0.25f, 1.0f, 1.0f}; // Blurple
+    P::SetCellNameAndDesc(material, "Inverted ability trigger cell", "Produces 1V when ability trigger not is pressed.");
+    P::materials_list[P::n_materials++] = material;
+}
