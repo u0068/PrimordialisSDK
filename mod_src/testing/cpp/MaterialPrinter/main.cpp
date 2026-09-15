98a74995ee04a474f9939ee1af3b12a432d615b3
u0068 <g3nio15@gmail.com>
2026-09-15 16:29:03 +0100

    material printer

---
+++ mod_src/testing/cpp/MaterialPrinter/main.cpp
@@ -0,0 +1,18 @@
+#include "plasmid_api.h"
+#include "material_printer.h"
+#include "tags_printer.h"
+
+// This function will be hooked to the game's init_materials_list function
+void OnInitMats() {
+    P::Next<void>(); // Call original function
+    if (P::IsThreadSafe()) // Make sure we are only on the main thread
+    {
+        print_material_properties();
+        print_material_tags();
+    }
+    P::LaneSync(); // Make all other threads wait for us to finish. Not sure if I actually need this.
+}
+
+void P::InitialiseMod() {
+    P::Hook<"init_materials_list">(OnInitMats); // Hook our OnInitMats function to the game's init_materials_list
+}
